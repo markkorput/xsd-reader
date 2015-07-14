262664c38774511ad467f0519ff720e9bafdf483
Mark van de Korput <dr.theman@gmail.com>
2015-07-14 19:19:39 +0200

    made byebug optional (for travis CI)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,15 @@
 # Licensed under the AGPLv3+ http://www.gnu.org/licenses/agpl.txt
 
 require 'rspec'
+require 'logger'
+
+begin
+  require 'byebug'
+rescue LoadError => e
+  Logger.new(STDOUT).warn("Could not load byebug, continuing without it")
+end
 
 CURRENT_DIR=File.dirname(__FILE__)
 $: << File.expand_path(CURRENT_DIR + "/../lib")
 
 require 'xsd_reader'
-require 'byebug'
